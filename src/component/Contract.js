bfc0443741c76e8418813128222f1c6da621bf7a
park <park129693@gmail.com>
2020-10-15 12:33:33 +0900

    file check ContractEnroll

--- src/component/Contract.js
+++ src/component/Contract.js
@@ -3,13 +3,7 @@ import PageHeader from "./PageHeader";
 
 //Contract Output
 const Contract = () => {
-<<<<<<< HEAD
   return <PageHeader>계약서 보기</PageHeader>;
-=======
-  return (
-    <PageHeader>계약서</PageHeader>
-  );
->>>>>>> d43011202b9f0a4413ae7d8ca1e1e9fa77d74cd6
 };
 
 export default Contract;
--- src/component/ContractEnroll.js
+++ src/component/ContractEnroll.js
@@ -8,59 +8,56 @@ const ContractEnroll = () => {
   return (
     <Container>
       <PageHeader>계약서 등록</PageHeader>
-<<<<<<< HEAD
-      <Form>
-=======
-      <Form style={{maxWidth:"500px", margin:"auto",}}>
->>>>>>> d43011202b9f0a4413ae7d8ca1e1e9fa77d74cd6
+
+      <Form style={{ maxWidth: "500px", margin: "auto" }}>
         <Form.Group controlId="formBasicLocation">
           <Form.Label> 계약 소재지 </Form.Label>
-            <PostFixInput
-              labelText="계약 소재지"
-              postfix=""
-              type="text"
-              placeholder="주소"
-            />
+          <PostFixInput
+            labelText="계약 소재지"
+            postfix=""
+            type="text"
+            placeholder="주소"
+          />
         </Form.Group>
 
         <Form.Group controlId="formBasicLandCategory">
           <Form.Label> 지목 </Form.Label>
-            <PostFixInput
-              labelText="지목"
-              postfix=""
-              type="text"
-              placeholder="지번"
-            />
+          <PostFixInput
+            labelText="지목"
+            postfix=""
+            type="text"
+            placeholder="지번"
+          />
         </Form.Group>
 
         <Form.Group controlId="formBasicLandArea">
           <Form.Label> 토지의 면적 </Form.Label>
-            <PostFixInput
-              labelText="토지의 면적"
-              postfix="㎡"
-              type="text"
-              placeholder="면적"
-            />
+          <PostFixInput
+            labelText="토지의 면적"
+            postfix="㎡"
+            type="text"
+            placeholder="면적"
+          />
         </Form.Group>
 
         <Form.Group controlId="formBasicBuildingPurpose">
           <Form.Label> 건물의 용도 </Form.Label>
-            <PostFixInput
-              labelText="건물의 용도"
-              postfix=""
-              type="text"
-              placeholder="종류"
-            />
+          <PostFixInput
+            labelText="건물의 용도"
+            postfix=""
+            type="text"
+            placeholder="종류"
+          />
         </Form.Group>
 
         <Form.Group controlId="formBasicBuildingArea">
           <Form.Label> 건물의 면적 </Form.Label>
-            <PostFixInput
-              labelText="부동산종류"
-              postfix="㎡"
-              type="text"
-              placeholder="면적"
-            />
+          <PostFixInput
+            labelText="부동산종류"
+            postfix="㎡"
+            type="text"
+            placeholder="면적"
+          />
         </Form.Group>
 
         <Form.Group controlId="formBasicPartOfLease">
@@ -70,22 +67,27 @@ const ContractEnroll = () => {
 
         <Form.Group controlId="formBasicPartOfLeaseArea">
           <Form.Label> 임대할 부분의 면적 </Form.Label>
-            <PostFixInput
-              labelText="부동산종류"
-              postfix="㎡"
-              type="text"
-              placeholder="면적"
-            />
+          <PostFixInput
+            labelText="부동산종류"
+            postfix="㎡"
+            type="text"
+            placeholder="면적"
+          />
         </Form.Group>
         <div>
+          <Form.Group
+            controlId="formBasicMonthly"
+            style={{ display: "inline-block", marginRight: "32px" }}
+          >
+            <Form.Check type="checkbox" label="월세" required />
+          </Form.Group>
 
-        <Form.Group controlId="formBasicMonthly" style={{display: "inline-block", marginRight: "32px"}}>
-          <Form.Check type="checkbox" label="월세" required />
-        </Form.Group>
-
-        <Form.Group controlId="formBasicResevations" style={{display: "inline-block",}}>
-          <Form.Check type="checkbox" label="전세" required />
-        </Form.Group>
+          <Form.Group
+            controlId="formBasicResevations"
+            style={{ display: "inline-block" }}
+          >
+            <Form.Check type="checkbox" label="전세" required />
+          </Form.Group>
         </div>
 
         <Form.Group controlId="formBasicPeriod">
@@ -95,42 +97,42 @@ const ContractEnroll = () => {
 
         <Form.Group controlId="formBasicSecurityDeposit">
           <Form.Label> 보증금 </Form.Label>
-            <PostFixInput
-              labelText="보증금"
-              postfix="만 원"
-              type="text"
-              placeholder="금액"
-            />
+          <PostFixInput
+            labelText="보증금"
+            postfix="만 원"
+            type="text"
+            placeholder="금액"
+          />
         </Form.Group>
 
         <Form.Group controlId="formBasicContractPrice">
           <Form.Label> 계약금 </Form.Label>
-            <PostFixInput
-              labelText="계약금"
-              postfix="만 원"
-              type="text"
-              placeholder="금액"
-            />
+          <PostFixInput
+            labelText="계약금"
+            postfix="만 원"
+            type="text"
+            placeholder="금액"
+          />
         </Form.Group>
 
         <Form.Group controlId="formBasicInterimPrice">
           <Form.Label> 중도금 </Form.Label>
-            <PostFixInput
-              labelText="중도금"
-              postfix="만 원"
-              type="text"
-              placeholder="금액"
-            />
+          <PostFixInput
+            labelText="중도금"
+            postfix="만 원"
+            type="text"
+            placeholder="금액"
+          />
         </Form.Group>
 
         <Form.Group controlId="formBasicBalance">
           <Form.Label> 잔금</Form.Label>
-            <PostFixInput
-              labelText="잔금"
-              postfix="만 원"
-              type="text"
-              placeholder="금액"
-            />
+          <PostFixInput
+            labelText="잔금"
+            postfix="만 원"
+            type="text"
+            placeholder="금액"
+          />
         </Form.Group>
 
         <Form.Group controlId="formBasicRent">
