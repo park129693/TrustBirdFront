ad5d3a4661a91baf9fefb8e22b219683b5b9e4dc
hansol <ekim569@gmail.com>
2020-10-15 18:04:38 +0900

    15commit

--- src/component/pages/ServiceIntro.js
+++ src/component/pages/ServiceIntro.js
@@ -3,13 +3,7 @@ import PageHeader from "./PageHeader";
 
 //Service Introduce
 const ServiceIntro = () => {
-<<<<<<< HEAD
   return <PageHeader>서비스 소개</PageHeader>;
-=======
-  return (
-    <PageHeader>서비스 소개</PageHeader>
-  );
->>>>>>> d43011202b9f0a4413ae7d8ca1e1e9fa77d74cd6
 };
 
 export default ServiceIntro;
